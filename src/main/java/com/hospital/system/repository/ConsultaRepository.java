f8b293485adc0c1193fdc5d95ec9b028f18295bc
João <109487647+s1ntos@users.noreply.github.com>
2025-05-30 11:04:44 -0300

    fix: organizando o projeto

--- src/main/java/com/hospital/system/repository/ConsultaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hospital.system.repository;
-
-import com.hospital.system.model.Consulta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
-}
